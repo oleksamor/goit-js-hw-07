f3dbca2cf83bbef6d612bb2ab6c437071e5f9a31
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-03-08 19:04:12 +0200

    task-1 upd

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,7 @@ const categories = document.querySelectorAll(".item")
 console.log("Number of categories :", categories.length);
 
 categories.forEach(element => {
-  console.log("element:", element);  
+ 
   console.log("Categorie:", element.firstElementChild.textContent);
   
   console.log("Elements:", element.lastElementChild.children.length);
