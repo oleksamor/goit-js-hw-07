9a7aa68439e73a897d4c0065441eeba5ff2ea5ae
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-03-11 22:12:18 +0200

    SOLVED t-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {email, password} = event.target.elements;
+
+    const info = {
+        email: email.value.trim(),
+        password: password.value.trim()
+    }
+    if (info.password === '' || info.email === '' ){alert('All form fields must be filled in')}
+     console.log(info);
+    
+   event.target.reset(); 
+}
