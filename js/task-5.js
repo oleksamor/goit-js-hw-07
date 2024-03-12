2933f09f4aa76f0af7a697ab96ae28a9140d54c5
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-03-12 10:02:38 +0200

    tASK--5 solved

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,20 @@
+const bgColor = document.querySelector(".widget");
+
+const btn = document.querySelector(".change-color");
+
+btn.addEventListener("click", handleClick);
+
+function handleClick(event) {
+const color = getRandomHexColor();
+const p = document.querySelector(".color");
+p.textContent = `${color}`;
+
+const body = document.querySelector("body");
+
+body.style.backgroundColor = color;
+ 
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
